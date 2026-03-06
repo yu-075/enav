23d43b4270cfda77b4cf91eb618d125609337c76
yu-075 <1067017140@qq.com>
2026-03-06 13:25:36 +0800

    chore: ignore colcon build artifacts

--- build/map2_navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/enav/src/map2_navigation
-# Build directory: /root/enav/build/map2_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
